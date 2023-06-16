b6aa6997e8c91d109ddcd84e9dfbb63e353e9b51
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-16 09:57:42 +0530

    Add to first added

---
+++ LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomeLikedList
+{
+    public class LinkedList<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        public void AddFirst(T Data)
+        {
+            Node<T> newNode = new Node<T>(Data);
+            if(head==null)
+            {
+                head=newNode;
+                tail=newNode;
+            }
+            else
+            {
+                newNode.next=head;
+                head=newNode;
+            }
+        }
+
+       /* public void AddLast(T Data)
+        {
+            Node<T> newNode =new Node<T>(Data);
+            if(head==null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail=newNode;
+            }
+        }
+
+        public void DeleteFirst()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No Data Avaliable");
+            }
+            else
+            {
+                Console.WriteLine(head.Data +"-> deleted successfully");
+                head =head.next;
+            }
+        }*/
+
+        public void Display()
+        {
+            Node<T> temp = head;
+            while(temp!=null)
+            {
+                Console.Write(temp.Data + " -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomeLikedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddFirst("A");
+            list.AddFirst("B");
+            list.AddFirst("C");
+            list.Display();
+            /*Console.WriteLine();
+            list.AddLast("z");
+            list.Display();
+            Console.WriteLine();
+            list.DeleteFirst();
+            list.Display();*/
+        }
+    }
+}
